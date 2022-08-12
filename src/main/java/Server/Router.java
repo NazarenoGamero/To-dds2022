1c4de423aa00a25ad84a3ac67188ac8b3982e971
23737k <82678482+23737k@users.noreply.github.com>
2022-08-11 21:39:00 -0300

    Update Router.java

--- src/main/java/Server/Router.java
+++ src/main/java/Server/Router.java
@@ -53,21 +53,3 @@ public class Router {
         });
     }
 }
-
-/*
-RepoLibros repo = new RepoLibros();
-    LibrosController controller = new LibrosController(repo);  //aca deberia instanciar todos mis controllers.
-    Gson gson = new Gson();           //AGREGO gson para que convierta y devuelva los objetos en json
-
-    Spark.get("/",(req,res)->"Hola");
-
-    Spark.get("/libros",controller::list,gson::toJson);  //delego en cada controller la accion a realizar por cada request
-
-    Spark.get("/libros/:id", controller::get,gson::toJson); //uso siempre el "::", nunca controller.unMetodo()
-
-    Spark.delete("/libros/:id", controller::delete);  //el :id hace que se guarde como valor esa parte del path
-                                                            //en este caso, guardar el id me servira para buscar el libro
-    Spark.post("/libros/",controller::create,gson::toJson); //el gson se encarga de transformar lo que devuelva el
-                                                                  //metodo a json
-  }
- */
