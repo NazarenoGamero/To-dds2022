58e1ba19d2ee7ca28bfa21447e1777e1f5d11923
Mauro Rossi <38499999+Maurorossi68@users.noreply.github.com>
2022-09-11 09:35:43 -0300

    Agregado ejemplo de consumo API

---
+++ src/main/java/dds/grupo3/api/service/impl/EmailServiceImpl.java
@@ -0,0 +1,14 @@
+package dds.grupo3.api.service.impl;
+
+import dds.grupo3.api.service.EmailService;
+
+public class EmailServiceImpl implements EmailService {
+
+	@Override
+	public void enviarMensaje(String mensaje, String email) {
+		// TODO Aca va la logica de envio de email
+		// Ver interface GmailAPI en el package dds.grupo3.api.service.client
+	}
+
+	
+}
