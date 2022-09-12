51078234b96fa7e872a8e6caf83885771ffb563a
GastonFUTN <gferreirosalvo@frba.utn.edu.ar>
2022-09-12 17:51:09 -0300

    Factores de emision controller, service y repo correccion

--- src/main/java/dds/grupo3/api/controller/impl/FactorEmisionControllerImpl.java
+++ src/main/java/dds/grupo3/api/controller/impl/FactorEmisionControllerImpl.java
@@ -1,10 +1,11 @@
 package dds.grupo3.api.controller.impl;
 
+import dds.grupo3.api.controller.FactorEmisionController;
 import dds.grupo3.api.service.FactorEmisionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
-public class FactorEmisionControllerImpl implements FactorEmisionControllerImpl {
+public class FactorEmisionControllerImpl implements FactorEmisionController {
     @Autowired
     FactorEmisionService factorEmisionService;
 
--- src/main/java/dds/grupo3/api/service/impl/FactorEmisionServiceImpl.java
+++ src/main/java/dds/grupo3/api/service/impl/FactorEmisionServiceImpl.java
@@ -1,20 +1,21 @@
 package dds.grupo3.api.service.impl;
 
 import dds.grupo3.api.repository.RepoFactorEmision;
-import dds.grupo3.clases.organizacion.Organizacion;
+import dds.grupo3.api.service.FactorEmisionService;
+import dds.grupo3.clases.medible.FactorEmision;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class FactorEmisionServiceImpl {
+public class FactorEmisionServiceImpl implements FactorEmisionService {
 
     @Autowired
     RepoFactorEmision repo;
 
     @Override
-    public List<Organizacion> buscarFactoresDeEmision() {
-        return repo.findById();
+    public List<FactorEmision> buscarFactoresDeEmision() {
+        return repo.findAll();
     }
 }
