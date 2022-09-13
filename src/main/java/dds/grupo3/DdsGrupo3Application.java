b41700c1ee171c4fe8fa2bec4fc9ed199488a054
kenti <016mki69@gmail.com>
2022-09-13 19:12:34 -0300

    Agrego envío de mails

--- src/main/java/dds/grupo3/DdsGrupo3Application.java
+++ src/main/java/dds/grupo3/DdsGrupo3Application.java
@@ -1,5 +1,6 @@
 package dds.grupo3;
 
+import dds.grupo3.api.service.impl.EmailServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +10,7 @@ public class DdsGrupo3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DdsGrupo3Application.class, args);
-		
+		new EmailServiceImpl().enviarMensaje("mopup2017@gmail.com", "Su huella de carbono es de 9000");
 	}
 
 }
--- src/main/java/dds/grupo3/api/service/impl/EmailServiceImpl.java
+++ src/main/java/dds/grupo3/api/service/impl/EmailServiceImpl.java
@@ -2,15 +2,62 @@ package dds.grupo3.api.service.impl;
 
 import dds.grupo3.api.service.EmailService;
 import org.springframework.stereotype.Service;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailServiceImpl implements EmailService {
-
 	@Override
-	public void enviarMensaje(String mensaje, String email) {
-		// TODO Aca va la logica de envio de email
-		// Ver interface GmailAPI en el package dds.grupo3.api.service.client
+	public void enviarMensaje(String texto, String destinatario) {
+
+		Properties prop = new Properties();
+		prop.put("mail.smtp.auth", true);
+		prop.put("mail.smtp.starttls.enable", "true");
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "587");
+
+		String remitente = "mihuellaapp2022@gmail.com";
+		String password = "zxcnzdxudkgljanm";
+
+		Session session = Session.getInstance(prop, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(remitente, password);
+			}
+		});
+		Message message = prepareMessage(session,remitente,destinatario,texto);
+		try {
+			Transport.send(message);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
 	}
 
-	
+	private static Message prepareMessage(Session session, String myAccount,String recipient,String texto) {
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(myAccount));
+			message.setRecipients(
+					Message.RecipientType.TO, InternetAddress.parse(recipient));
+			message.setSubject("Notificacion Mi Huella App");
+			message.setText(texto);
+			return message;
+		}
+		catch (Exception exception){
+			Logger.getLogger(EmailServiceImpl.class.getName()).log(Level.SEVERE,null,exception);
+		}
+		return null;
+	}
+
 }
+
