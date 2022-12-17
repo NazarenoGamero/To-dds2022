570adee03a5a7ffc28c19b2d4574c4f0dc849f0a
Nazareno Gamero <82052356+NazarenoGamero@users.noreply.github.com>
2022-12-17 08:00:06 -0300

    logout1

--- src/main/java/dds/grupo3/api/controller/impl/LoginController.java
+++ src/main/java/dds/grupo3/api/controller/impl/LoginController.java
@@ -3,12 +3,13 @@ package dds.grupo3.api.controller.impl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class LoginController {
 		
+	
 		@GetMapping("/login")
 		public String iniciarSesion() {
 			return "login";
 		}
+		
 }
